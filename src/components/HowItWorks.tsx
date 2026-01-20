3289a7f3a7432a75ecebcbd56bcdb3a58e0887c8
mafedelahoz <m.delah@uniandes.edu.co>
2026-01-20 10:16:52 -0500

    change desembolsa

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -21,7 +21,7 @@ const steps = [
     number: "03",
     title: "Recibe tu dinero",
     description:
-      "Welli te desenbolsa en máximo 72 horas. El paciente paga en cuotas.",
+      "Welli te desembolsa en máximo 72 horas. El paciente paga en cuotas.",
   },
 ];
 
